75e09584e0c53a00ea405584c4f56735eaf65bc1
mmartin440 <105955175+mmartin440@users.noreply.github.com>
2022-08-31 17:01:20 -0700

    Delte please

--- steam-clone/components/Layout.js
+++ steam-clone/components/Layout.js
@@ -1,23 +1,11 @@
-// import Header from "./Header"
-// import Footer from "./Footer"
-
-import Link from 'next/link'
-
+import Header from "./Header"
+import Footer from "./Footer"
 const Layout = ({children}) => {
   return (
       <div>
-        <header className='bg-black text-white h-20'>
-          <Link href='/'>Store</Link>
-          <Link href='/'> Community</Link>
-          <Link href='/'>Chat</Link>
-          <Link href='/'>Support</Link>
-        </header>
+        <Header />
           <div>{children}</div>
-        <footer className="bg-[#171A21] h-[182px]">
-            <div>
-                blah
-            </div>
-        </footer>
+        <Footer />
       </div>
   )
 }
