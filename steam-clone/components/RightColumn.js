3e4c81652852b35bbed40e56cb6a4cd0fc38211d
Gerard San Juan <49268224+Tiwala@users.noreply.github.com>
2022-09-02 15:40:57 -0500

    changed globals.css, put padding and margin into *

--- steam-clone/components/RightColumn.js
+++ steam-clone/components/RightColumn.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 export const RightColumn = () => {
   return (
-    <div>RightColumn</div>
+    <div className="mt-[16px] overflow-hidden w-[308px] ml-[14px] float-right flex flow-col">
+        RightColumn
+    </div>
   )
 }
 
