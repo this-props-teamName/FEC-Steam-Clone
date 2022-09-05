03494e2236cc874db2d9dd45f5f7b09ee083bf32
Alexander Cobble <acobble@Alexanders-MacBook-Pro.local>
2022-09-04 21:54:13 -0700

    added copy write information at the bottom of the system requirements section.

--- steam-clone/components/GameSystemRequirements.js
+++ steam-clone/components/GameSystemRequirements.js
@@ -8,6 +8,7 @@ const GameSystemRequirements = () => {
     .then(res => console.log(res.data[0]));
 
   return (
+    <>
     <div className='relative text-[14px] text-[#acb2b8]'>GameSystemRequirements
       <div className='mt-[30px] overflow-hidden max-h-max font-sans'>
         <h2 className='text-[14px] uppercase text-[#fff] pt-[2px] tracking-[0.03em] leading-[26px] bg-[url("https://store.cloudflare.steamstatic.com/public/images/v6/maincol_gradient_rule.png")] bg-no-repeat bg-left-bottom'>System Requirements</h2>
@@ -60,34 +61,44 @@ const GameSystemRequirements = () => {
                 <strong className='font-sans font-normal text-[10px] text-[#bcc6cd] uppercase inline-block mr-[4px] leading-[18px] list-none list-inside'>Recommended:</strong>
                 <br />
                 <ul className='list-none ml-0 leading-[18px] list-inside text-[12px] font-serif text-[#acb2b8]'>
-                  <li className='my-[2px] mx-0 list-outside list-none leading-[18px] '>Requires a 64-bit processor and operating system</li>
+                  <li className='my-[2px] mx-0 list-outside list-none leading-[18px] '>
+                    Requires a 64-bit processor and operating system
+                    <br />
+                  </li>
                   <li className='my-[2px] mx-0 list-outside list-none leading-[18px] text-[12px] font-normal font-serif text-[#acb2b8]'>
                     <strong className='text-[#61686D] text-[12px] mr-[4px] font-normal mb-[2px] uppercase font-sans inline-block list-none leading-[18px]'>OS:</strong>
                     Windows 10
+                    <br />
                   </li>
                   <li className='my-[2px] mx-0 list-outside list-none leading-[18px] text-[12px] font-normal font-serif text-[#acb2b8]'>
                     <strong className='text-[#61686D] text-[12px] mr-[4px] font-normal mb-[2px] capitalize font-sans inline-block list-none leading-[18px]'>Processor:</strong>
-                    Intel Core i7-8700 OR AMD Ryzen 5 3600
+                    Intel Core i7-8700 OR AMD Ryzen 5 <br/>3600
+                    <br />
                   </li>
                   <li className='my-[2px] mx-0 list-outside list-none leading-[18px] text-[12px] font-normal font-serif text-[#acb2b8]'>
                     <strong className='text-[#61686D] text-[12px] mr-[4px] font-normal mb-[2px] capitalize font-sans inline-block list-none leading-[18px]'>Memory:</strong>
                     16 GB RAM
+                    <br />
                   </li>
                   <li className='my-[2px] mx-0 list-outside list-none leading-[18px] text-[12px] font-normal font-serif text-[#acb2b8]'>
                     <strong className='text-[#61686D] text-[12px] mr-[4px] font-normal mb-[2px] capitalize font-sans inline-block list-none leading-[18px]'>Graphics:</strong>
                     NVIDIA GeForce 1080 Ti or AMD RX 5700 XT
+                    <br />
                   </li>
                   <li className='my-[2px] mx-0 list-outside list-none leading-[18px] text-[12px] font-normal font-serif text-[#acb2b8]'>
                     <strong className='text-[#61686D] text-[12px] mr-[4px] font-normal mb-[2px] capitalize font-sans inline-block list-none leading-[18px]'>DirectX:</strong>
                     Version 12
+                    <br />
                   </li>
                   <li className='my-[2px] mx-0 list-outside list-none leading-[18px] text-[12px] font-normal font-serif text-[#acb2b8]'>
                     <strong className='text-[#61686D] text-[12px] mr-[4px] font-normal mb-[2px] capitalize font-sans inline-block list-none leading-[18px]'>Storage:</strong>
                     85 GB available space
+                    <br />
                   </li>
                   <li className='my-[2px] mx-0 list-outside list-none leading-[18px] text-[12px] font-normal font-serif text-[#acb2b8]'>
                     <strong className='text-[#61686D] text-[12px] mr-[4px] font-normal mb-[2px] capitalize font-sans inline-block list-none leading-[18px]'>Additional Notes:</strong>
                     SSD, 1080p/60 fps, High Quality Settings, Upscale Quality Setting
+                    <br />
                   </li>
                 </ul>
               </ul>
@@ -95,7 +106,24 @@ const GameSystemRequirements = () => {
           </div>
         </div>
       </div>
+      {/* div for autocollapse section */}
+      <div className='hidden absolute left-0 right-0 bottom-0 h-[73px] z-[1] bg-top bg-repeat-x'>
+        <div className='absolute right-0 bottom-0 h-[20px] leading-[20px] cursor-pointer text-[#537ca6] text-[10px] pr-[12px] bg-right-center bg-no-repeat'></div>
+      </div>
     </div>
+    <div className='relative text-[14px] text-[#acb2b8] font-sans'>
+      <div className='max-h-max overflow-hidden text-[14px] text-[#acb2b8] font-sans'>
+        <div className='text-[#566168] text-[11px] mb-[10px] mt-[20px] font-sans'>  
+          <p className='text-[#566168] text-[11px] font-sans leading-normal '>
+          HOGWARTS LEGACY software © 2022 Warner Bros. Entertainment Inc. Developed by Avalanche Software. WIZARDING WORLD and HARRY POTTER Publishing Rights © J.K. Rowling. PORTKEY GAMES, HOGWARTS LEGACY, WIZARDING WORLD AND HARRY POTTER characters, names and related indicia © and ™ Warner Bros. Entertainment Inc.
+          <br />
+          <br />
+          WARNER BROS. GAMES LOGO, WB SHIELD: ™ & © Warner Bros. Entertainment Inc. (s22)
+          </p>
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
