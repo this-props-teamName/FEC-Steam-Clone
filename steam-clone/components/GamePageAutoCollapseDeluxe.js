72bba46828f08d65685e6c294b7827815b3e39cc
mmartin440 <105955175+mmartin440@users.noreply.github.com>
2022-09-05 18:26:04 -0700

    added on click to the read more in the Deluxe image section

--- steam-clone/components/GamePageAutoCollapseDeluxe.js
+++ steam-clone/components/GamePageAutoCollapseDeluxe.js
@@ -1,13 +1,21 @@
-
-
-
+import React, {useState} from 'react'; 
+ 
 const GamePageAutoCollapseDeluxe = () => {
+
+  const [readMoreClick, setReadMoreClick] = useState(false); 
+
+  const digiDeFullScreenClass = () => {
+      return `${readMoreClick ? '' : 'max-h-[300px] overflow-hidden'}`
+  }
+  const readMoreClass = () => {
+    return `${readMoreClick ? 'hidden' : 'block absolute left-0 right-0 bottom-0 h-[73px] z-[1]'}`
+  }
 
   return (
     <div className="pb-[20px] relative">
       GamePageAutoCollapseDeluxe
       {/* If you take away the max-h-[300px] it will show the entire thing */}
-      <div className="max-h-[300px] overflow-hidden ">
+      <div className={digiDeFullScreenClass()}>
         <div className="leading-[1.5rem] text-[14px] mt-[30px] overflow-hidden font-sans font-normal ">
           <h2 className="text-[14px] uppercase text-[#fff] mt-[0px] mx-[0px] mb-[10px] tracking-[0.03rem] font-normal pt-2px  ">
             Pre-Purchase Offer
@@ -36,8 +44,8 @@ const GamePageAutoCollapseDeluxe = () => {
             <br />
           </p>
         </div>
-        <div className="block absolute left-0 right-0 bottom-0 h-[73px] z-[1]  ">
-          <div className="absolute right-0 bottom-0 h-[20px] leading-[20px] cursor-pointer text-[#537ca6] text-[10px] pr-[12px] hover:text-[#fff] ">
+        <div className={readMoreClass()}>
+          <div className="absolute right-0 bottom-0 h-[20px] leading-[20px] cursor-pointer text-[#537ca6] text-[10px] pr-[12px] hover:text-[#fff] " onClick={()=> setReadMoreClick(true)}>
             READ MORE
             {/* need to add a gradient here but cant add since image is not http */}
           </div>
